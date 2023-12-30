78601e3a46383c8122d65ebc01326aeb86b141b1
SIV <fathersson1302@gmail.com>
2023-12-30 16:56:20 +0530

    Updated rdp_test.c. Fixed issues from earlier. Grammar needs change for proper RDP.

--- exp_3_recursive_descent_parser/rdp_test.c
+++ exp_3_recursive_descent_parser/rdp_test.c
@@ -1,6 +1,10 @@
-// Tried a symmetrical string of 0s & 1s using RDP using 0S0 & 1S1 but it got stuck in if loop. Need to find problem & fix later.
+/*
+Tried a symmetrical string of 0s & 1s using RDP using 0S0 & 1S1 but it got stuck
+in if loop. Need to find problem & fix later.
+*/
 
 #include <stdio.h>
+#include <string.h>
 
 char s[100];
 int p = 0;
@@ -10,7 +14,8 @@ int S();
 int main(void) {
   printf("Hello World, enter a string:\n");
   scanf("%s", s);
-  if (S()) {
+  strcat(s, "$");
+  if (S() && s[p] == '$') {
     printf("\nString accepted\n");
   } else {
     printf("\nInvalid string\n");
@@ -19,35 +24,46 @@ int main(void) {
 }
 
 int S() {
-  if (s[p] == '0') {
-    printf("yes1");
+  if (p == (strlen(s) / 2)) {
+    return 1;
+    /*
+      Nor proper RDP implementation, not sure if this can be done using RDP.
+    */
+  } else if (s[p] == '0') {
+    // printf("yes1");
     p++;
     if (S()) {
       if (s[p] == '0') {
-        printf("yes2");
+        // printf("yes2");
         p++;
         return 1;
-      } else
+      } else {
         p--;
         return 0;
-    } else
+      }
+    } else {
       p--;
-    return 0;
+      return 0;
+    }
   } else if (s[p] == '1') {
-    printf("yes1");
+    // printf("yes1");
     p++;
     if (S()) {
       if (s[p] == '1') {
-        printf("yes2");
+        // printf("yes2");
         p++;
         return 1;
-      } else
+      } else {
         p--;
         return 0;
-    } else
+      }
+    } else {
       p--;
-    return 0;
-  } else
-    return 1;
-  // epsilon
+      return 0;
+    }
+  } else {
+    p--;
+    return 0; // return 1; ?
+    // epsilon
+  }
 }
